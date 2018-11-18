937cd023b8e2934725687c9da1bc4a6c3f01b3ba
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-18 20:07:19 +0900

    Use double-quote instead of single one

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,4 +1,4 @@
-root_dir = File.expand_path('../../..', __FILE__)
+root_dir = File.expand_path("../../..", __FILE__)
 
 file File.join(ENV["HOME"], ".Xresources") do
   content File.read(File.join(root_dir, "config", ".Xresources"))
@@ -16,12 +16,12 @@ execute "brew doctor" do
   only_if "which brew"
 end
 
-include_cookbook 'packages'
-include_cookbook 'cask'
+include_cookbook "packages"
+include_cookbook "cask"
 
-include_cookbook 'karabiner'
-include_cookbook 'git'
-include_cookbook 'zsh'
-include_cookbook 'emacs'
+include_cookbook "karabiner"
+include_cookbook "git"
+include_cookbook "zsh"
+include_cookbook "emacs"
 
-include_cookbook 'ricty'
+include_cookbook "ricty"
