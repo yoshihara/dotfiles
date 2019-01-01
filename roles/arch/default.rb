d82c4872d75e9f4f9e0c98f816dba46c0c8969a7
yoshihara <594134+yoshihara@users.noreply.github.com>
2019-01-01 23:43:26 +0900

    Add role for arch

---
+++ roles/arch/default.rb
@@ -0,0 +1,11 @@
+root_dir = File.expand_path("../../..", __FILE__)
+
+file File.join(ENV["HOME"], ".ssh", "config") do
+  content File.read(File.join(root_dir, "config", ".ssh", "config"))
+end
+
+# TODO: include_cookbook "yay"
+
+include_cookbook "git"
+include_cookbook "zsh"
+include_cookbook "emacs"
