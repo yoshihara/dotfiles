0656fe04266f6b8580af8b9ebf3d2759af5a1dd0
yoshihara <594134+yoshihara@users.noreply.github.com>
2019-01-01 23:40:28 +0900

    Avoid to run w/o ssh config

--- init.sh
+++ init.sh
@@ -2,5 +2,10 @@
 
 set -eu
 
+if [ ! -e "${HOME}/.ssh" ]; then
+  echo "ERROR: not found ssh key. Please create one by ssh-keygen."
+  exit 1
+fi
+
 ./setup_mitamae.sh
 ./bin/mitamae local $@ lib/recipe.rb
